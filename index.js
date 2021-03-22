a09a267d64bf1af30654323749adce4ccfa320a2
maheenur13 <differentmahee13@gmail.com>
2021-03-23 04:10:42 +0600

    more interesting things

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express')
-
+var cors = require('cors')
 const app = express()
-
+app.use(cors())
+const users=['Mahee','Suzon','Shohaib','Ashik','Saiful','Sakib Ahmed']
 app.get('/',(req,res)=>{
-    res.send('Thank you for sending!Okay?')
+    const fruit = {
+        product :'ada',
+        price:'240tk'
+    }
+    res.send(fruit)
+})
+app.get('/users/:id',(req,res)=>{
+    const userId=req.params.id;
+    console.log(req.params);
+    const name = users[userId];
+    res.send({userId,name});
+    // res.send()
+})
+app.get('/fruits/banana',(req,res)=>{
+    res.send({fruit:'banana',quantity: 2000,price : 4000})
 })
 
 app.listen(3000,()=>console.log('listening to port 3000!'))
