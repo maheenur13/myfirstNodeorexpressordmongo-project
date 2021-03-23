b8033aa505f18a507f94f40804c697582c7b9ecb
maheenur13 <differentmahee13@gmail.com>
2021-03-23 17:06:13 +0600

    add mehtod

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 var cors = require('cors')
+const bodyParser = require('body-parser')
 const app = express()
 app.use(cors())
+app.use(bodyParser.json())
 const users=['Mahee','Suzon','Shohaib','Ashik','Saiful','Sakib Ahmed']
 app.get('/',(req,res)=>{
     const fruit = {
@@ -19,6 +21,13 @@ app.get('/users/:id',(req,res)=>{
 })
 app.get('/fruits/banana',(req,res)=>{
     res.send({fruit:'banana',quantity: 2000,price : 4000})
+})
+app.post('/addUser',(req,res)=>{
+    console.log('okay done!',req.body);
+    //save to database
+    const user = req.body;
+    user.id=55;
+    res.send(user);
 })
 
 app.listen(3000,()=>console.log('listening to port 3000!'))
